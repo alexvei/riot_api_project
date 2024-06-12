c7b539c3979440d65abb1a96e42f9750cdfb6c51
alexvei <balorianosx@gmail.com>
2024-06-12 18:49:26 +0100

    More cleanup.

--- main.py
+++ main.py
@@ -77,26 +77,32 @@ class Scraper:
         return sorted_dict
 
 
-    # Get PUUID of accounts by summonerID
-    def puuids(self, num_players: int, number_of_games: int, all_players_list: list):
-        """
-        Provides the summonerId, the PUUID, the gameID and finally passes that information to get the items list.
-        """
-        list_of_people_puuids = []
+    def summonerIDs(self, num_players: int, all_players_list: list)-> list:
         list_of_summonerId = []
-        stored_games = []
-
-        # Check if major tier or not then get the required amount of summonerIDs.
         if self.major:
             for player_index in range(num_players):
                 list_of_summonerId.append(all_players_list['entries'][player_index]['summonerId'])
         else:
             for player_index in range(num_players):
                 list_of_summonerId.append(all_players_list[player_index]['summonerId'])
+        return list_of_summonerId
+
+
+    # Get PUUID of accounts by summonerID
+    def puuids(self, num_players: int, number_of_games: int, all_players_list: list):
+        """
+        Provides the summonerId, the PUUID, the gameID and finally passes that information to get the items list.
+        """
+        list_of_people_puuids = []
+        list_of_summoner_Id = self.summonerIDs(num_players, all_players_list)
+        stored_games = []
+
+        # Check if major tier or not then get the required amount of summonerIDs.
+
 
         # Using the summonerIDs, get the PUUIDs and also get the requested amount of games from those PUUIDs.
         for i in range(num_players):
-            puuid_finder = self.api_urls(f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/{list_of_summonerId[i]}?')
+            puuid_finder = self.api_urls(f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/{list_of_summoner_Id[i]}?')
             list_of_people_puuids.append(puuid_finder['puuid'])
             games = self.api_urls(f'https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{list_of_people_puuids[i]}/ids?start=0&count={number_of_games}&')
             stored_games.append(games)
@@ -109,8 +115,13 @@ class Scraper:
                 for participant_number in range(10):
                     if list_of_people_puuids[players_game_container] in player[participant_number]['puuid']:
                         self.list_maker(participant_number, player)
-
         return self.items_purchased_list
+
+    # def single_player(self, game_name:str, tag_line: str):
+    #     player_info = self.api_urls(f'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}?')
+    #     print(player_info['puuid'])
+
+        
 
 
 # -k : use api key 
@@ -120,14 +131,21 @@ class Scraper:
 # -v : default is 2, from 1 to 100
 parser = argparse.ArgumentParser()
 parser.add_argument("-k", "--key", type=str, help="Use API key.", required=True)
-parser.add_argument("-t", "--tier", type=str, help="Type a tier.", required=True)
+parser.add_argument("-t", "--tier", type=str, default= 'challenger', help="Type a tier.")
 parser.add_argument("-d", "--division", type=str, help="Type a divison.", default='one')
 parser.add_argument("-p", "--people", type=int, default=2, help="Enter the total amount of players to get data from.")
 parser.add_argument("-v", "--volume", type=int, default=2, help="Enter the amount of games to get data from.")
+parser.add_argument("-gm", "--game_name", type=str, help="enter the game name of a player.")
+parser.add_argument("-tl", "--tag_line", type=str, help="enter the tag line of a player.")
+
 args = parser.parse_args()
 
 s1 = Scraper(args.key)
 
+# if args.game_name and args.tag_line:
+#     s1.single_player(args.game_name, args.tag_line)
+
+# else:   
 final_list = s1.puuids(args.people, args.volume, s1.tier_choice(args.tier, args.division))
 
 item_frequency = s1.list_to_dict(final_list)
